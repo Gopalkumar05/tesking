5a7038e6fe77155312765f46af21c15b56d03ec5
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-08-30 16:03:24 +0530

    Update next.config.ts

--- astrology-landing/next.config.ts
+++ astrology-landing/next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "imgcdn.stablediffusionweb.com",
-        pathname: "/**",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "imgcdn.stablediffusionweb.com",
+  //       pathname: "/**",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
